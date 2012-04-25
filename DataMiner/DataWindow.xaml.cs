e76634ec967c127ef409cff78c3525c6cf5b3267
unknown <kearneyandy@gmail.com>
2012-04-25 00:50:16 -0700

    start on tabs and windows

---
+++ DataMiner/DataWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DataMiner
+{
+    /// <summary>
+    /// Interaction logic for DataWindow.xaml
+    /// </summary>
+    public partial class DataWindow : Window
+    {
+        public DataWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void newSearch(object sender, RoutedEventArgs e)
+        {
+            SeachBox search = new SeachBox();
+            search.Owner = this;
+            search.Show();
+            
+        }
+        public void Search(string stockSymbol)
+        {
+            ((Window1)this.Owner).newSearch(stockSymbol);
+        }
+
+        public void dealWithResults(string stockSymbol)//otherstuff
+        {
+            TabItem newTab = new TabItem();
+            newTab.Header = stockSymbol;
+            //UpdateContent
+            this.StockSymbolTabs.Items.Add(newTab);
+        }
+    }
+}
---
+++ DataMiner/SeachBox.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DataMiner
+{
+    /// <summary>
+    /// Interaction logic for SeachBox.xaml
+    /// </summary>
+    public partial class SeachBox : Window
+    {
+        public SeachBox()
+        {
+            InitializeComponent();
+        }
+
+        private void searchButtonClick (object sender, RoutedEventArgs e)
+        {
+            ((DataWindow)this.Owner).Search(this.SearchBox.Text);
+            this.Close();
+        }
+        private void searchBoxMouseDown(object sender, RoutedEventArgs e)
+        {
+            this.SearchBox.Text = "";
+        }
+    }
+}
